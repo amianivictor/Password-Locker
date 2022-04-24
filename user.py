88f802cb2dcdd626cdf55c852db46724eec9d93e
amianivictor <khahonjeri.amiani@student.moringaschool.com>
2022-04-24 09:52:34 +0300

    add user_list

--- user.py
+++ user.py
@@ -3,4 +3,10 @@ class user:
     Class that generates new instances of user
     """
 
-    
+    user_list = []
+    def __init__(self,user_name,password):
+         self.user_name = user_name
+         self.password = password
+
+
+   
